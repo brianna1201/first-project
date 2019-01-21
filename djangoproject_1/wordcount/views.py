f9d2a8237ef385ae8533b8ac198ab1b777b4e54a
brianna1201 <qhqo0729@gmail.com>
2019-01-21 15:49:31 +0900

    additional fuction

--- djangoproject_1/wordcount/views.py
+++ djangoproject_1/wordcount/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import operator
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -10,8 +10,8 @@ def about(request):
 def count(request):
      full_text = request.GET['fulltext']
  
-     word_list = full_text.split()
- 
+     word_list = full_text.lower().split()
+     
      word_dictionary = {}
  
      for word in word_list:
@@ -21,5 +21,8 @@ def count(request):
          else:
              # add to the dictionary
              word_dictionary[word] = 1
- 
-     return render(request, 'count.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()})
+
+     sorted_dictionary = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+     print(sorted_dictionary)
+
+     return render(request, 'count.html', {'fulltext': full_text, 'total': len(word_list),'sorted_dictionary':sorted_dictionary})
